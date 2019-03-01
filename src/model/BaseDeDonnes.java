b2e272222e9ffe2e4f2b809195d9dfaacbdbafe5
Ghouibi <ghouibi.ghassen@gmail.com>
2019-03-01 04:33:04 +0100

    add model #9

---
+++ src/model/BaseDeDonnes.java
@@ -0,0 +1,35 @@
+/**
+* 
+*/
+public abstract class BaseDeDonnes {
+
+    /**
+    * Default constructor
+    */
+    public BaseDeDonnes() {
+    }
+
+    /**
+     * 
+     */
+    public int host;
+
+    /**
+     * 
+     */
+    public int port;
+
+
+
+    /**
+     * 
+     */
+    public void connexion() {
+
+    }
+
+    public void chargerlesinformations(){
+        //Charger Les Articles,Les Fournisseur,Détaillant et les derniers actions
+    }
+
+}
